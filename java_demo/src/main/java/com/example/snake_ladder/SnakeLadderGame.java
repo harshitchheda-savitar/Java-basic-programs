7db5fb563e4655322558e80de8c3adddfca78ab2
harshit <harshit@futurewise.co.in>
2020-09-24 13:12:16 +0530

    Welcome message for game with the start position done

---
+++ java_demo/src/main/java/com/example/snake_ladder/SnakeLadderGame.java
@@ -0,0 +1,8 @@
+package com.example.snake_ladder;
+
+public class SnakeLadderGame {
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("Welcome to the game... This is a single plyer game!!! You current position is: " + position);
+	}
+}
