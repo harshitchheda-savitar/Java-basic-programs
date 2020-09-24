8273c361d1503fa75695086a0274bdb3c29b179c
harshit <harshit@futurewise.co.in>
2020-09-24 13:02:46 +0530

    employee monthly wage calculation based on threshold values implemented

--- java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
+++ java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
@@ -11,24 +11,29 @@ public class EmployeeWage {
 		final int FULL_DAY_HOUR = 8;
 		final int HALF_DAY_HOUR = 4;
 		final int WORKING_DAYS_IN_MONTH = 20;
+		final int TOTAL_WORKING_HOURS = 100;
 		
 		int empDailyWage = 0;
+		int workDayCount = 0;
+		int empWorkHrs = 0;
 		
-		for(int n=1; n<=WORKING_DAYS_IN_MONTH; n++) {
+		while((workDayCount < WORKING_DAYS_IN_MONTH) && (empWorkHrs < TOTAL_WORKING_HOURS)) {
 			int empStatus = random.nextInt(3);
+			++workDayCount;
 			switch(empStatus) {
 			case FULL_TIME:
+				empWorkHrs += FULL_DAY_HOUR;
 				empDailyWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
-				System.out.println("Day " + n +": The Employee is a FULL-TIME employee");
+				System.out.println("Day " + workDayCount +": The Employee is a FULL-TIME shift");
 				break;
 			case PART_TIME:
+				empWorkHrs += HALF_DAY_HOUR;
 				empDailyWage += WAGE_PER_HOUR * HALF_DAY_HOUR;
-				System.out.println("Day " + n +": The Employee is a PART-TIME employee");
+				System.out.println("Day " + workDayCount +": The Employee is a PART-TIME shift");
 				break;
 			default :
-				empDailyWage += 0;
-				System.out.println("Day " + n +": The Employee is absent");
-			}		
+				System.out.println("Day " + workDayCount +": The Employee is absent");
+			}
 		}
 		
 		
