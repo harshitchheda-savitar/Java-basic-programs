fd76b3329b2ecdf3017df8c3ead6ca0635bb2bd5
harshit <harshit@futurewise.co.in>
2020-09-24 12:26:04 +0530

    Employee daily wage computation based on employee shift implemented

--- java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
+++ java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
@@ -5,17 +5,27 @@ import java.util.Random;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		Random random = new Random();
-		final int PRESENT = 1;
+		final int FULL_TIME = 1;
+		final int PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int FULL_DAY_HOUR = 8;
+		final int HALF_DAY_HOUR = 4;
 		
-		int isPresent = random.nextInt(2);
+		int empStatus = random.nextInt(3);
 		int empDailyWage;
-		if(isPresent == PRESENT)
+		if(empStatus == FULL_TIME) {
 			empDailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-		else
+			System.out.println("The Employee is a FULL-TIME employee");
+		}
+		else if(empStatus == PART_TIME) {
+			empDailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+			System.out.println("The Employee is a PART-TIME employee");
+		}
+		else {
 			empDailyWage = 0;
+			System.out.println("The Employee is absent");
+		}
 			
-		System.out.println("Employee was " + (isPresent == PRESENT ? "PRESENT" : "ABSENT") + " so the person's daily wage is " + empDailyWage);	
+		System.out.println("The person's daily wage is " + empDailyWage);	
 	}
 }
