3803764963ae5cfa1d6f2a0a88ceddfa1ca7f11a
harshit <harshit@futurewise.co.in>
2020-09-24 14:11:43 +0530

    gameplay for single player implemented

--- java_demo/src/main/java/com/example/snake_ladder/SnakeLadderGame.java
+++ java_demo/src/main/java/com/example/snake_ladder/SnakeLadderGame.java
@@ -5,9 +5,44 @@ import java.util.Random;
 public class SnakeLadderGame {
 	public static void main(String[] args) {
 		
-		Random random = new Random();
+		Random diceValue = new Random();
+		Random optionValue = new Random();
+		final int WIN_STEP = 100;
+		final int START_POSITION = 0;
+		final int LADDER = 1;
+		final int SNAKE = 2;
+		int currentPosition = START_POSITION;
+		int optionFlag;
+		int diceCount;
 		
-		int diceValue = random.nextInt(6) + 1;
-		System.out.println("Dice rolled and the value was: " + diceValue);
+		System.out.println("currentPosition : " + currentPosition);
+		System.out.println("-------------------------------------------------");
+
+		
+		while(currentPosition >=START_POSITION && currentPosition < WIN_STEP) {
+			diceCount = diceValue.nextInt(6) + 1;
+			optionFlag = optionValue.nextInt(3);
+			
+			System.out.println("dice value: "+ diceCount);
+			
+			switch(optionFlag) {
+			case LADDER:
+				currentPosition += ((currentPosition + diceCount) <= WIN_STEP) ? diceCount : 0;
+				System.out.println("Found a ladder.... currentPosition : " + currentPosition);
+				break;
+			case SNAKE:
+				currentPosition -= ((currentPosition - diceCount) >= START_POSITION) ? diceCount : currentPosition;
+				System.out.println("Bitten by a snake.... currentPosition : " + currentPosition);
+				break;
+			default:
+				System.out.println("No movement.... currentPosition : " + currentPosition);
+				break;
+				
+			}
+			System.out.println("-------------------------------------------------");
+		}
+		
+		//System.out.println("Dice count: " + count);
+		
 	}
 }
