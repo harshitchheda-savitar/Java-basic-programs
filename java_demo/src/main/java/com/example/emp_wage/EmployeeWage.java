29a65ac5e881f45cd44a08b542ac6fda39c559ba
harshit <harshit@futurewise.co.in>
2020-09-24 12:15:20 +0530

    employee daily wage computation part inplemented

--- java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
+++ java_demo/src/main/java/com/example/emp_wage/EmployeeWage.java
@@ -6,11 +6,16 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		Random random = new Random();
 		final int PRESENT = 1;
+		final int WAGE_PER_HOUR = 20;
+		final int FULL_DAY_HOUR = 8;
 		
 		int isPresent = random.nextInt(2);
+		int empDailyWage;
 		if(isPresent == PRESENT)
-			System.out.println("Employee is Present");
+			empDailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
 		else
-			System.out.println("Employee is absent");
+			empDailyWage = 0;
+			
+		System.out.println("Employee was " + (isPresent == PRESENT ? "PRESENT" : "ABSENT") + " so the person's daily wage is " + empDailyWage);	
 	}
 }
