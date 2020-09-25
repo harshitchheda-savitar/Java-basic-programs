899f4f171dbd4c08bd8953b06688bd0315d8fcd1
harshit <harshit@futurewise.co.in>
2020-09-25 14:14:20 +0530

    Added a welcome message for the AddressBook

---
+++ java_demo/src/main/java/com/example/address_book/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.example.address_book;
+
+//AddressBookMain class
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// Print out a welcome message
+		System.out.println("Welcome to the Address book portal !!!");
+	}
+}
